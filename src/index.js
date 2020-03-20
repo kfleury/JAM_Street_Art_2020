92d198b96661915f2e746a428bb3c8fb6d5e9b68
Arthur FOUQUET <afouquet@repeteurwifi-3f04.home>
2020-03-20 19:28:15 +0100

    az

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const app = new PIXI.Application();
+
+document.body.appendChild(app.view);
+
+app.loader.add('bunny', 'run.png').load(
+    (loader,
+     resources) => {
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    app.stage.addChild(bunny);
+
+    app.ticker.add(() => {
+        bunny.rotation += 0.01;
+    });
+});
--- src/killian/app.js
+++ src/killian/app.js
@@ -1,5 +1,3 @@
-import * as PIXI from 'pixi.js'
-
 const app = new PIXI.Application();
 
 document.body.appendChild(app.view);
